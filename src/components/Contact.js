952beb3e13be927bace5f3ca684623e6fec731b6
Mitchell Wood <mitchellwood11@gmail.com>
2023-02-03 16:39:46 -0600

    fixed email link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -48,7 +48,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href = 'mailto: mitchellwood11@gmail.com' className="text-indigo-400 leading-relaxed">
                 mitchellwood11@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
