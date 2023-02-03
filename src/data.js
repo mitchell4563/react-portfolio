f101a80050e183fcf79d60e19fa5ed4591ebab25
Mitchell Wood <mitchellwood11@gmail.com>
2023-02-03 15:21:46 -0600

    initial build

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: "Note taker app",
+    subtitle: "Made in Node with an Express.js server",
+    description:
+      "Allows you to save and edit notes",
+    image: "./Note-taker-app.png",
+    link: "https://intense-meadow-56790.herokuapp.com/",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "Simple JS/HTML/CSS application",
+    description:
+      "Weather forecast provider that gives you both current weather and the 5 day forecast for a given location",
+    image: "./weather-dashboard.png",
+    link: "https://mitchell4563.github.io/Weather-Dashboard/",
+  },
+  {
+    title: "Bounty Hunter's guild",
+    subtitle: "MVC Framework with authentication",
+    description:
+      "Bounty Hunting Website set in the star wars universe",
+    image: "./BHG.png",
+    link: "https://nameless-stream.herokuapp.com/login",
+  },
+  {
+    title: "Work Day Scheduler",
+    subtitle: "Note taker made using local storage",
+    description:
+      "schedule from 9am-5pm with color changing depending on the time",
+    image: "./Work-Day-Scheduler.png",
+    link: "https://mitchell4563.github.io/Weekday-Planner/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "MySQL",
+  "Node",
+  "GraphQL",
+  "MongoDB",
+];
